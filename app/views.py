680dee5ddbe0d849bb21a0f689097b7d0a05267a
Nico Julian <NicoGiuliani@users.noreply.github.com>
2022-12-03 17:21:59 -0800

    Added logic to perform Caesar cipher

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from datetime import datetime
 
 # Create your views here.
 def index(request):
@@ -6,6 +7,19 @@ def index(request):
 
 def encode(request):
     if request.method == 'POST':
-        plaintext_message = request.POST['plaintext_message']
-        encoded_message = plaintext_message[::-1]
+        plaintext_message = request.POST['plaintext_message'].upper()
+        shift = 5
+
+        english_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        encoded_message = list(plaintext_message[:])
+
+        for i in range(len(plaintext_message)):
+            current_character = plaintext_message[i]
+            alphabet_index = english_alphabet.find(current_character)
+            if alphabet_index >= 0:
+                encoded_message[i] = english_alphabet[(alphabet_index + shift) % 26]
+
+        encoded_message = ''.join(encoded_message)
+
         return render(request, 'encode.html', {'encoded_message': encoded_message})
+
