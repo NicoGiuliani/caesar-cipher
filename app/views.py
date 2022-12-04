03803d62cfec23b72e0d314e42e589b854952990
Nico Julian <NicoGiuliani@users.noreply.github.com>
2022-12-03 18:30:18 -0800

    Added decode logic and text area

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ from datetime import datetime
 def index(request):
     return render(request, 'index.html')
 
+
 def encode(request):
     if request.method == 'POST':
         plaintext_message = request.POST['plaintext_message'].upper()
@@ -22,4 +23,22 @@ def encode(request):
         encoded_message = ''.join(encoded_message)
 
         return render(request, 'encode.html', {'encoded_message': encoded_message})
+
+
+def decode(request):
+    encoded_message = request.POST['encoded_message'].upper()
+    plaintext_message = list(encoded_message[:])
+
+    english_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    shift = 5
+
+    for i in range(len(encoded_message)):
+        current_character = encoded_message[i]
+        alphabet_index = english_alphabet.find(current_character)
+        if alphabet_index >= 0:
+            plaintext_message[i] = english_alphabet[(alphabet_index - shift) % 26]
+
+    plaintext_message = ''.join(plaintext_message)
+
+    return render(request, 'decode.html', {'plaintext_message': plaintext_message})
 
