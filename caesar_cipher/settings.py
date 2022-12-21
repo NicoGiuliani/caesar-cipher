6b6db28b2e0b7e0cb4c9b79e338bc64eb6f8c2b6
Nico Julian <NicoGiuliani@users.noreply.github.com>
2022-12-20 17:52:14 -0800

    Switched to postgresql

--- caesar_cipher/settings.py
+++ caesar_cipher/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'caesar_cipher.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'TcajJIT1Mhds9eYuqZP9',
+        'HOST': 'containers-us-west-146.railway.app',
+        'PORT': '6870',
     }
 }
 
